ed725451373d085531e17dd7390f5f486c70e800
iwahara_aoi <iwahara.aoi@alhinc.jp>
2025-05-01 14:52:27 +0900

    first commit

--- src/chapter6/controller/SettingServlet.java
+++ src/chapter6/controller/SettingServlet.java
@@ -117,12 +117,17 @@ public class SettingServlet extends HttpServlet {
         }
         if (StringUtils.isEmpty(account)) {
             errorMessages.add("アカウント名を入力してください");
-        } else if (20 < account.length()) {
-            errorMessages.add("アカウント名は20文字以下で入力してください");
         }
-        if (StringUtils.isEmpty(password)) {
-            errorMessages.add("パスワードを入力してください");
+
+
+
+        if (!StringUtils.isEmpty(password) && password.length() < 6) {
+            errorMessages.add("パスワードは6文字以上で入力してください");
         }
+
+
+
+
         if (!StringUtils.isEmpty(email) && (50 < email.length())) {
             errorMessages.add("メールアドレスは50文字以下で入力してください");
         }
